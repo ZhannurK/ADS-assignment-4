0c95e4dfe18eef2daa92d0d333aea7db4a9e1d3c
Zhannur Kainedin <230030@astanait.edu.kz>
2024-05-22 15:44:41 +0500

    finished class

---
+++ src/MyGraph.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyGraph<Vertex> {
+    private final boolean undirected;
+    private final Map<Vertex, List<Vertex>> map = new HashMap<>();
+
+    public MyGraph() {
+        this(true);
+    }
+
+    public MyGraph(boolean undirected) {
+        this.undirected = undirected;
+    }
+
+    public void addVertex(Vertex vertex) {
+        map.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(Vertex source, Vertex destination) {
+        if (hasVertex(source)) {
+            addVertex(source);
+        }
+        if (hasVertex(destination)) {
+            addVertex(destination);
+        }
+        if(hasEdge(source, destination)) {
+            addEdge(source, destination);
+        }
+        map.get(source).add(destination);
+        if (undirected) {
+            map.get(destination).add(source);
+        }
+    }
+
+    public int getVertexCount() {
+        return map.size();
+    }
+
+    public int getEdgeCount() {
+        int count = 0;
+        for (List<Vertex> vertexList : map.values()) {
+            count += vertexList.size();
+        }
+        if (undirected) {
+            count = count / 2;
+        }
+        return count;
+    }
+
+    public boolean hasVertex(Vertex vertex) {
+        return !map.containsKey(vertex);
+    }
+
+    public boolean hasEdge(Vertex source, Vertex destination) {
+        if (hasVertex(source)) {
+            return false;
+        }
+        return map.get(source).contains(destination);
+    }
+
+    public Iterable<Vertex> adjacencyList(Vertex source) {
+        if (hasVertex(source)) {
+            return null;
+        }
+        return map.get(source);
+    }
+}
--- src/WeightGraph.java
+++ src/WeightGraph.java
@@ -1,9 +1,80 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WeightGraph <Vertex> {
-    private Map<Vertex, List<Edge<Vertex>>> map = new HashMap<>();
+    private final Map<Vertex, List<Edge<Vertex>>> map = new HashMap<>();
+    private final boolean undirected;
 
+    public WeightGraph(){
+        this(true);
+    }
+
+    public WeightGraph(boolean undirected) {
+        this.undirected = undirected;
+    }
+
+    public void addVertex(Vertex vertex) {
+        if (!hasVertex(vertex)) {
+            map.put(vertex, new ArrayList<>());
+        }
+    }
+
+    public void addEdge(Vertex source, Vertex destination, double weight) {
+        if(hasVertex(source)){
+            addVertex(source);
+        }
+        if(hasVertex(destination)){
+            addVertex(destination);
+        }
+        if(hasEdge(source, destination, weight)){
+            addEdge(source, destination, weight);
+        }
+        map.get(source).add(new Edge<>(source, destination, weight));
+        if(undirected){
+           map.get(destination).add(new Edge<>(source, destination, weight));
+        }
+    }
+
+    public int getVerticesCount(){
+        return map.size();
+    }
+
+    public int getEdgesCount(){
+        int count = 0;
+        for(Vertex v : map.keySet()){
+            count += map.get(v).size();
+        }
+        if (undirected){
+            count /= 2;
+        }
+        return count;
+    }
+
+    public boolean hasVertex(Vertex vertex){
+        return !map.containsKey(vertex);
+    }
+
+    public boolean hasEdge(Vertex source, Vertex destination, double weight){
+        if(hasVertex(source)){
+            return false;
+        }
+        return map.get(source).contains(new Edge<>(source, destination, weight));
+    }
+
+    public List<Edge<Vertex>> adjacentList(Vertex vertex){
+        if(hasVertex(vertex)){
+            return null;
+        }
+        List<Edge<Vertex>> vectices = new ArrayList<>();
+        for(Edge<Vertex> edge : map.get(vertex)){
+            vectices.add(edge);
+        }
+        return vectices;
+    }
+
+    public List<Edge<Vertex>> getEdges(Vertex vertex){
+        if(hasVertex(vertex)){
+            return null;
+        }
+        return map.get(vertex);
+    }
 }
-
